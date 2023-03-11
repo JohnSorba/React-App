a2b975bd51fa199f9022e3ab0636a6ee307060ed
Daniel Ndanema <dannytech001@gmail.com>
2023-03-11 20:24:04 +0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1>Hello World!</h1>
+);
+// root.render(<h1>Test</h1>);
